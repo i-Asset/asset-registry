898bbc353b6963bd54dc87c774ccebfc17b4623c
Dietmar Glachs <dglachs@gmail.com>
2020-07-24 13:29:09 +0200

    add HasKind functionality when setting/getting Asset

--- src/main/java/at/srfg/iot/aas/service/basys/event/GetAssetEvent.java
+++ src/main/java/at/srfg/iot/aas/service/basys/event/GetAssetEvent.java
@@ -3,7 +3,7 @@ package at.srfg.iot.aas.service.basys.event;
 import at.srfg.iot.aas.basic.Asset;
 
 public class GetAssetEvent extends GetIdentifiableElementEvent<Asset, org.eclipse.basyx.aas.metamodel.map.parts.Asset>
-	implements GetReferable, GetIdentifiable, GetHasKind, GetHasDataSpecification {
+	implements GetReferable, GetIdentifiable, GetHasDataSpecification {
 
 	public GetAssetEvent(Asset asset) {
 		super(newAsset(), asset);
