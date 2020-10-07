6da15fc27d4c9541f9309bf0f07e3af21f87d27c
Dietmar Glachs <dglachs@gmail.com>
2020-10-07 15:58:31 +0200

    directory controller for lookup

---
+++ src/main/java/at/srfg/iot/aas/web/controller/directory/AssetDirectoryController.java
@@ -0,0 +1,81 @@
+package at.srfg.iot.aas.web.controller.directory;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+
+import at.srfg.iot.aas.basic.AssetAdministrationShell;
+import at.srfg.iot.aas.basic.Identifier;
+import at.srfg.iot.aas.basic.Submodel;
+import at.srfg.iot.aas.basic.directory.AssetAdministrationShellDescriptor;
+import at.srfg.iot.aas.basic.directory.SubmodelDescriptor;
+import at.srfg.iot.aas.common.referencing.IdType;
+import at.srfg.iot.aas.service.registry.RegistryService;
+import at.srfg.iot.aas.service.registry.RegistryWorker;
+import at.srfg.iot.api.AssetDirectoryAPI;
+
+@RestController
+public class AssetDirectoryController implements AssetDirectoryAPI {
+	@Autowired
+	private RegistryService registry;
+	
+	@Autowired
+	private RegistryWorker worker;
+
+	@Override
+	public Optional<AssetAdministrationShellDescriptor> lookup(String aasIdentifier) {
+		Identifier id = new Identifier(aasIdentifier);
+		Optional<AssetAdministrationShell> aas = registry.getAssetAdministrationShell(id);
+		if (aas.isPresent()) {
+			AssetAdministrationShellDescriptor desc = new AssetAdministrationShellDescriptor(aas.get());
+			return Optional.of(desc);
+		}
+		return Optional.empty();
+	}
+
+	@Override
+	public Optional<SubmodelDescriptor> lookup(String aasIdentifier, String submodelIdentifier) {
+		Identifier id = new Identifier(submodelIdentifier);
+		if ( id.getIdType().equals(IdType.IdShort)) {
+			Optional<AssetAdministrationShell> aas = registry.getAssetAdministrationShell(id);
+			if (aas.isPresent()) {
+				Optional<Submodel> sub = aas.get().getSubmodel(id);
+				if ( sub.isPresent()) {
+					return Optional.of(new SubmodelDescriptor(sub.get()));
+				}
+			}
+			return Optional.empty();
+			
+		}
+		Optional<Submodel> submodel = registry.getSubmodel(id);
+		if ( submodel.isPresent()) {
+			return Optional.of(new SubmodelDescriptor(submodel.get()));
+		}
+		return Optional.empty();
+	}
+
+	@Override
+	public Optional<AssetAdministrationShell> register(AssetAdministrationShellDescriptor shell) {
+		return worker.registerAdministrationShell(shell);
+	}
+
+	@Override
+	public Optional<Submodel> register(String aasIdentifier, SubmodelDescriptor model) {
+		return worker.registerSubmodel(aasIdentifier, model);		
+	}
+
+	@Override
+	public void unregister(String aasIdentifier) {
+		
+		
+	}
+
+	@Override
+	public void unregister(String aasIdentifier, List<String> submodelIdentifier) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
