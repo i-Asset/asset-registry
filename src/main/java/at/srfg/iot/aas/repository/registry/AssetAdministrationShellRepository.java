45d03b84ea9d9d37ca884c628f7383d11046c2aa
Dietmar Glachs <dglachs@gmail.com>
2020-12-16 16:34:04 +0100

    find AAS by its parent (derivedFrom)

--- src/main/java/at/srfg/iot/aas/repository/registry/AssetAdministrationShellRepository.java
+++ src/main/java/at/srfg/iot/aas/repository/registry/AssetAdministrationShellRepository.java
@@ -1,6 +1,8 @@
 package at.srfg.iot.aas.repository.registry;
+import java.util.List;
+
 import at.srfg.iot.aas.basic.AssetAdministrationShell;
 
 public interface AssetAdministrationShellRepository extends IdentifiableRepository<AssetAdministrationShell> {
-
+	List<AssetAdministrationShell> findByDerivedFromElement(AssetAdministrationShell parent);
 }
